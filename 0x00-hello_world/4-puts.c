049388a53b0e5df9b2c19b6ff5781a166c2d7be2
Pakus <mrocran1@gmail.com>
2023-02-16 07:02:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line of code using the puts function.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
