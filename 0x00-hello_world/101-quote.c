c04fb4506578a0783a505af90a13fcdb9ee3e1d9
Pakus <mrocran1@gmail.com>
2023-02-16 08:22:05 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  * Return: 1 (Sucess)
  */
 int main(void)
-{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
