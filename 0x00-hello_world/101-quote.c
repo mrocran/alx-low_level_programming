db27814f00c7af050277edad7379f089746586c8
Pakus <mrocran1@gmail.com>
2023-02-16 08:17:30 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that print a line of code to the standard error.
+ *
+ * Return: 1 (Sucess)
+ */
+int main(void)
+{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
