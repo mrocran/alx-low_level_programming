6906fb1bf92cd86e91ce853e1ba982bbd495b657
Pakus <mrocran1@gmail.com>
2023-02-16 07:09:54 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program tthat print a line of code using the pritf function.
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
