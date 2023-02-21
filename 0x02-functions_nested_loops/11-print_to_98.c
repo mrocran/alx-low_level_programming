79d78505c51448271ccca20676a0a87020e749f1
Pakus <mrocran1@gmail.com>
2023-02-21 15:03:27 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,14 +13,14 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n--);
 		}
-		printf("%d", n);
+		printf("%d ", n);
 	}
 	else
 	{
 		while (n < 98)
 		{
 			printf("%d, ", n++);
-		printf("%d\n", n);
+		printf("%d ", n);
 		}
 	}
 }
