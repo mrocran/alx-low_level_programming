71c2b941f7824afd6b174a417ad4c00080dd60c1
Pakus <mrocran1@gmail.com>
2023-02-22 03:34:43 -0800

    100-times_table.c:41

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_taime_table - prints times table of the input,
+ * starting with 0
+ * @n: value to be printed
+ */
+void print_times_table(int n)
+{
+
+	int x, y, z;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+			for (y = 1; y <= n; y++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				z = x * y;
+				if (z <= 99)
+					_putchar(' ');
+				if (z <= 9)
+					_putchar(' ');
+				if (z >= 100)
+				{
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+				}
+				else if (z <= 99 && z >= 10)
+				{
+					_putchar((z / 10) + '0');
+				}
+				_putchar((z % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
