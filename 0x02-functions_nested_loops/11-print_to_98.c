df2c449660d4ed5810531fbabcc17f84647271bd
Pakus <mrocran1@gmail.com>
2023-02-22 01:54:35 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,36 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n <= 98)
 	{
-		while (n > 98)
+		for (; n <= 98; n++)
 		{
-			printf("%d, ", n--);
+			if (n == 98)
+			{
+				printf("%d",  n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		printf("%d ", n);
 	}
 	else
 	{
-		while (n < 98)
+		for (; n >= 98; n--)
 		{
-			printf("%d, ", n++);
-		printf("%d ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d ", n);
+			}
 		}
 	}
 }
