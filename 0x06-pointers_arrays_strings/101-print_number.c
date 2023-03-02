4bf27aeb3cd715672a86cd0a382ede2ae22409e4
Pakus <mrocran1@gmail.com>
2023-03-02 13:22:00 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,11 +13,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		n1 = - n;
 	}
 	if (n1 / 10 != 0)
 	{
-		print_number(n1 / 10)
+		print_number(n1 / 10);
 	}
 	_putchar((n1 % 10) + '0');
 }
