d1cba6c86edf6d435a3505c60ed2c36afa85f6a0
Pakus <mrocran1@gmail.com>
2023-03-06 02:14:44 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring.
+ * @haystack: string input
+ * @needle: substring
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	
+}
