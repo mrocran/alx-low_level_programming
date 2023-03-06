667af719a61265873b754be840f34321df149fa3
Pakus <mrocran1@gmail.com>
2023-03-06 03:26:40 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,5 +8,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	
+	for (; *haystack != '\0'; haystck++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+		if (*two == '\0')
+			return (haystack);
+	}
+	return (NULL);
 }
