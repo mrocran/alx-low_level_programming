b4b8e9d8f1685f1d4a5441e8dee35c27def94825
Pakus <mrocran1@gmail.com>
2023-03-08 08:52:35 -0800

    101-wildcmp.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to be squared
+ * Return: squared n
+ */
+int _sqrt_recursion(int n)
+{
+	
+}
