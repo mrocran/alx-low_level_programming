3283ae9bdccee774d496ff6983b0d962fa1baba6
Pakus <mrocran1@gmail.com>
2023-03-10 05:21:59 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints the name of the program
  * @argc: number of arguments
- * @argv: array of aguements
+ * @argv: array of arguments
  *
  * Return: Always 0
  */
