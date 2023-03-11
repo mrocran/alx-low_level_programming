571054f17358f2736ab69b9c9a089d334e58a375
Pakus <mrocran1@gmail.com>
2023-03-11 03:59:25 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line of code using the puts function.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
