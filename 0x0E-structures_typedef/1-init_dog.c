0df90b1fd6d91b53e6b5fba34379136834c39509
Pakus <mrocran1@gmail.com>
2023-03-21 13:21:32 -0700

    dog.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 	/**
-	 * init_dog - initializes a variable of type struct dog
+	 * main: initializes a variable of type struct dog
 	 * @d: pointer to struct dog to initialize
 	 * @name: name to initialize
 	 * @age: age to initialize
